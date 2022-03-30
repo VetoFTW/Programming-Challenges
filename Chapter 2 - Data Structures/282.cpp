03fea73a7d1e55367138e61854f5d227c9bdb9da
Martin Czarnecki <martin.czarnecki02@gmail.com>
2022-03-30 17:56:37 -0400

    Add files via upload

---
+++ Chapter 2 - Data Structures/282.cpp
@@ -0,0 +1,183 @@
+/*  @JUDGE_ID: 1274804  
+    Problem Number: 110202/10315
+    Language: C++
+    "Poker Hands"
+*/
+
+/* @BEGIN_OF_SOURCE_CODE */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+using std::string;
+using std::vector;
+
+struct Card{
+    unsigned int value;
+    char suite;
+};
+
+bool sortCardComparator(const Card &firstCard, const Card &secondCard){
+    return firstCard.value < secondCard.value;
+}
+
+struct Player{
+    string name;
+    vector<Card> hand;
+    unsigned int highCard;
+    vector<unsigned int> handResults;
+};
+
+Card makeCard(char value, char suite){
+    unsigned int cardValue;
+    switch (value){
+        case 'T':
+            cardValue = 10;
+            break;
+        case 'J':
+            cardValue = 11;
+            break;
+        case 'Q':
+            cardValue = 12;
+            break;
+        case 'K':
+            cardValue = 13;
+            break;
+        case 'A':
+            cardValue = 14;
+            break;
+        default:
+            cardValue = (unsigned int) value - 48;
+            break;
+    }
+    return Card {cardValue, suite};
+}
+
+unsigned int isHighCard(Player &p){
+    return p.hand[4].value;
+}
+
+unsigned int isPair(Player &p){
+    for(unsigned int i = 4; i > 0; i--)
+        if(p.hand[i].value == p.hand[i-1].value)
+            return p.hand[i].value;
+    return 0;
+}
+
+unsigned int isTwoPair(Player &p){
+    unsigned int firstPair = isPair(p);
+    if(firstPair == 0) return 0;
+    unsigned int secondPair = 0;
+    for(unsigned int i = 0; i < p.hand.size()-1; i++)
+        if(p.hand[i].value == p.hand[i+1].value && p.hand[i].value != firstPair)
+            return p.hand[i].value;
+    return 0;
+}
+
+unsigned int isThreeOfAkind(Player &p){
+    for(unsigned int i = 0; i <= 2; i++)
+        if(p.hand[i].value == p.hand[i+1].value && p.hand[i].value == p.hand[i+2].value)
+            return p.hand[i].value;
+    return 0;
+}
+
+unsigned int isStraight(Player &p){
+    for(unsigned int i = 0; i < p.hand.size()-1; i++)
+        if(p.hand[i].value+1 != p.hand[i+1].value)
+            return 0;
+    return p.hand[4].value;
+}
+
+unsigned int isFlush(Player &p){
+    for(Card c : p.hand) 
+        if(c.suite != p.hand[0].suite)
+            return 0;
+    return p.hand[4].value;
+}
+
+unsigned int isFullHouse(Player &p){
+    if(isPair(p) == 0) return 0;
+    return isThreeOfAkind(p);
+}
+
+unsigned int isFourOfAkind(Player &p){ // return 0 if none is found, else return the value of the highest card
+    for(unsigned int i = 0; i <= 1; i++)
+        if(p.hand[i].value == p.hand[i+1].value && p.hand[i].value == p.hand[i+2].value && p.hand[i].value == p.hand[i+3].value)
+            return p.hand[i].value;
+    return 0;
+}
+
+unsigned int isStraightFlush(Player &p){ // return 0 if none is found, else return the value of the highest card
+    if(isFlush(p) == 0)
+        return 0;
+    return isStraight(p);
+}
+
+vector<unsigned int> calculateHand(Player &p){
+    std::sort(p.hand.begin(), p.hand.end(), sortCardComparator);
+    p.highCard = isHighCard(p);
+    vector<unsigned int> handResults;
+    handResults.push_back(isStraightFlush(p));
+    handResults.push_back(isFourOfAkind(p));
+    handResults.push_back(isFullHouse(p));
+    handResults.push_back(isFlush(p));
+    handResults.push_back(isStraight(p));
+    handResults.push_back(isThreeOfAkind(p));
+    handResults.push_back(isTwoPair(p));
+    handResults.push_back(isPair(p));
+    handResults.push_back(isHighCard(p));
+    return handResults;
+}
+
+unsigned int isTieOrWinner(vector<Card> &set1, vector<Card> &set2){ // For Some Reason, a for loop dose not work her.
+    if(set1[4].value != set2[4].value) return (set1[4].value > set2[4].value ? 1 : 2);
+    if(set1[3].value != set2[3].value) return (set1[3].value > set2[3].value ? 1 : 2);
+    if(set1[2].value != set2[2].value) return (set1[2].value > set2[2].value ? 1 : 2);
+    if(set1[1].value != set2[1].value) return (set1[1].value > set2[1].value ? 1 : 2);
+    if(set1[0].value != set2[0].value) return (set1[0].value > set2[0].value ? 1 : 2);
+    return 0;
+}
+
+void returnWinner(Player &player1, Player &player2){
+    unsigned int winner = 0; // 0 tie, 1 player 1, 2 player 2.
+    player1.handResults = calculateHand(player1);
+    player2.handResults = calculateHand(player2);
+    
+    for(unsigned int i = 0; i < player1.handResults.size(); i++){
+        if(player1.handResults[i] == player2.handResults[i] && player1.handResults[i] == 0)
+            continue;
+        if(player1.handResults[i] != player2.handResults[i]){
+            winner = player1.handResults[i] > player2.handResults[i] ? 1 : 2;
+            break;
+        }
+    }
+
+    if(winner == 0) winner = isTieOrWinner(player1.hand, player2.hand);
+    if(winner == 0) std::cout << "Tie." << '\n';
+    if(winner == 1) std::cout << player1.name << " wins." << '\n';
+    if(winner == 2) std::cout << player2.name << " wins." << '\n';
+}
+
+int main(){
+    string inputLine;
+
+    while(std::getline(std::cin, inputLine, '\n')){
+        Player player1;
+        Player player2;
+        player1.name = "Black";
+        player2.name = "White";
+
+        std::stringstream tableCards(inputLine);
+        char suit, value;
+        
+        while(tableCards >> value >> suit)
+            if(player1.hand.size() != 5)  player1.hand.push_back(makeCard(value, suit));
+            else                          player2.hand.push_back(makeCard(value, suit));
+
+        returnWinner(player1, player2);
+    }    
+}
+
+/* @END_OF_SOURCE_CODE */
